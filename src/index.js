490557abe631de20cf5b25c57ca42f7c4592402f
kmontalvom <kmontalvom@ucic.pe>
2020-04-13 01:30:50 -0500

    add eslint and babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld.jsx'
+import HelloWorld from './components/HelloWorld.jsx';
 
-ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
