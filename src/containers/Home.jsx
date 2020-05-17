d79635cde1a15e69da25d1bf5e0bb26005c5905a
kmontalvom <kmontalvom@ucic.pe>
2020-05-17 13:01:28 -0500

    added login and register structures + router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import Header from '../components/Header.jsx';
 import Search from '../components/Search.jsx';
 import Categories from '../components/Categories.jsx';
 import CarouselItem from '../components/CarouselItem.jsx';
 import Carousel from '../components/Carousel.jsx';
-import Footer from '../components/Footer.jsx';
 import useInitialState from '../hooks/useInitialState.js';
 
 import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initIalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   console.log(initialState);
 
   return (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
 
       {
@@ -51,9 +48,8 @@ const App = () => {
         </Carousel>
       </Categories>
 
-      <Footer />
-    </div>
+    </>
   );
 };
 
-export default App;
+export default Home;
